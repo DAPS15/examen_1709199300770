ef51027a2561bc16486e6fc76f40093ac0df05db
Alejandra Paz <pdinaalejandra@gmail.com>
2017-06-06 18:56:41 -0600

    Rota que devuelve un objeto Json

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Examen' });
+});
+
+router.get('/api/datos', function(req,res,next){
+  var datos = [
+                {
+  "numero de cuenta":"1709199300770",
+  "nombre completo":"Dina Alejandra Paz Suazo",
+  "colores":"pdinaalejandra@hotmail.com"
+ 
+    }
+              ];
+  res.json(datos);
+
 });
 
 module.exports = router;
